b8142d136212f1725ebc7eb14bb418821ed75316
gschultzy <greg.schultz@emc.com>
2017-09-26 13:27:40 +1000

    task_run_tests.sh updates

--- concourse/task_run_tests.sh
+++ concourse/task_run_tests.sh
@@ -7,7 +7,7 @@ ls -al ci-repo/concourse
 ls -al ci-project/
 
 # Setup container to run & test node app
-sudo apt-get -y update
+apt-get -y update
 apt-get install curl
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-get install -y nodejs
