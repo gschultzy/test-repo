9f8cf60b23fec133c19d722ec96ebdb618ddcb22
gschultzy <greg.schultz@emc.com>
2017-10-05 15:27:40 +1100

    updates

--- concourse/task_run_tests.sh
+++ concourse/task_run_tests.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 set -e # fail fast
-set -x # print commands for debugging
+# set -x # print commands for debugging
 
-
+# Get the cf application name from the manifest file
 appname=$(grep 'name:' ci-project/manifest.yml | awk '{print $3}')
-echo $appname
 
-# # Connect to cf
-# cf login -a $api -u $username -p $password -o $organization -s $space
+# Connect to cf
+cf login -a $api -u $username -p $password -o $organization -s $space
 
-# # open shh connection to app container
-# echo "Connecting to $appname ..."
-# cf ssh $appname -c "set -e && 
-# set -x && 
-# export PATH=$PATH:/home/vcap/deps/0/node/bin/ && 
-# alias npm='node /home/vcap/deps/0/node/lib/node_modules/npm/bin/npm-cli.js' && 
-# cd app/ && 
-# npm install --only=dev && 
-# npm test"
+# open SSH connection to app container and run tests
+echo .
+echo "SSH to application $appname ..."
+echo .
+cf ssh $appname -c "set -e && 
+set -x && 
+export PATH=$PATH:/home/vcap/deps/0/node/bin/ && 
+alias npm='node /home/vcap/deps/0/node/lib/node_modules/npm/bin/npm-cli.js' && 
+cd app/ && 
+npm install --only=dev && 
+npm test"
 
 # cf ssh greg_test -c "set -x && set -e && export PATH=$PATH:/home/vcap/deps/0/node/bin/ && alias npm='node /home/vcap/deps/0/node/lib/node_modules/npm/bin/npm-cli.js' && cd app/ && npm install --only=dev && npm test"
 
